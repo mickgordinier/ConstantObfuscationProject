06f2228c4dd7de4c5066681c7d8fec5f99b18541
Mick Gordinier <mickg@eecs583a.eecs.umich.edu>
2025-04-20 17:08:41 -0400

    Removing build folder for gitignore

--- build/obfuscationPass/CMakeFiles/install-ObfuscationPass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-ObfuscationPass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-ObfuscationPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
